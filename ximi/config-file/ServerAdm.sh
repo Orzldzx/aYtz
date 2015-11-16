2e65b23ec00a4061c84cd1882201e870d7e27900
1un.ximi <orzldzx@gmail.com>
2015-11-17 06:55:17 +0800

    修复bug

--- ximi/config-file/ServerAdm.sh
+++ ximi/config-file/ServerAdm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 1un
-# 2015-10-22
+# 2015-11-11
 # 整合服务启动/停止/重启/重载功能
 
 #变量
@@ -26,13 +26,13 @@ Help(){
 Print(){
     if [ $? -eq 0 ]; then
     	if [[ $Ptype -eq 1 ]]; then
-                printf "\033[32m %-100s %-7s succeed ...\033[0m\n" "[ ${1} ]" ${2}
+                printf "\033[32m %-100s %-7s succeed     ...\033[0m\n" "[ ${1} ]" ${2}
         elif [[ $Ptype -eq 2 ]]; then
                 printf "\033[33m %-100s running %-12s...\033[0m\n" "[ ${1} ]" ${2}
         fi
     else
         if [[ $Ptype -eq 1 ]]; then
-            printf "\033[31m %-100s %-7s failed ...\033[0m\n" "[ ${1} ]" ${2}
+            printf "\033[31m %-100s %-7s failed      ...\033[0m\n" "[ ${1} ]" ${2}
         elif [[ $Ptype -eq 2 ]]; then
             printf "\033[36m %-100s down    %-12s...\033[0m\n" "[ ${1} ]" ${2}
         fi
@@ -56,7 +56,7 @@ Start(){
     else
         cd /svndata/svr/${1}/bin && ./${1} &> /dev/null
     fi
-    sleep 8
+    sleep 0.5
     pgrep ${1} &> /dev/null
     Print ${1} start
 }
@@ -65,11 +65,16 @@ Start(){
 Stop(){
     local Ptype=1
     pkill -9 ${1:0:15}
-    sleep 0.5
+    sleep 0.2
     [[ $(pgrep ${1:0:15}|wc -l) -eq 0 ]] || pkill -9 ${1:0:15}
     Print ${1} stop
-    sleep 0.5
-    ClearShm
+}
+
+#重启服务
+Restart(){
+    Stop ${1}
+    sleep 0.2
+    Start ${1}
 }
 
 #重载服务
@@ -83,7 +88,7 @@ Reload(){
 #查看状态
 Status(){
     local Ptype=2
-    for Ps in ${1}
+    for Ps in ${1:0:15}
     do
         Time=$(ps -o comm,etime -p $(pgrep ${Ps}) 2>/dev/null|grep -v COMMAND|awk '{print $2}')
         pgrep ${Ps} > /dev/null
@@ -97,7 +102,11 @@ Case(){
         start)
             Start ${2} ;;
         stop)
-            Stop ${2} ;;
+            Stop ${2}
+            sleep 0.5
+            ClearShm ;;
+        restart)
+            Restart ${2} ;;
         reload)
             Reload ${2} ;;
         status)
@@ -115,25 +124,14 @@ Case(){
 
 #main
 if [[ ${2} = "all" ]]; then
-    if [[ ${1} = "restart" ]]; then
-	for Ser in $(awk '{print $1}' $Mod)
-	do
-	    Case stop ${Ser}
-	done
-	echo -e "\033[35;05m >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>          <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\033[0m"
-	for Ser in $(awk '{print $1}' $Mod)
-	do
-	    Case start ${Ser}
-	done
-    else
-        for Ser in $(awk '{print $1}' $Mod)
-        do
-            Case ${1} ${Ser}
-        done
-    fi
+    for Ser in $(awk '{print $1}' $Mod)
+    do
+	[[ ${1} = "reload" && ${Ser} = "autorestart" ]] && continue
+        Case ${1} ${Ser}
+    done
 else
     if [[ $(grep ${2} $Mod|wc -l) -ge 1 ]]; then
-        [[ ${1} = "restart" ]] && { Case stop ${2} ; Case start ${2} ; } || Case ${1} ${2}
+        Case ${1} ${2}
     else
          echo "${2} not found ..."
     fi
